3e82f86a0fb523e28a9d31a63232034215954212
NGO-Forum <support@ngoforum.org.kh>
2025-11-25 11:56:13 +0700

    feature : add the feature for the linkedin url

--- app/app.vue
+++ app/app.vue
@@ -313,7 +313,7 @@
 
               <!-- Social Icons -->
               <div class="flex flex-wrap gap-4 sm:gap-5">
-                <a href="https://www.linkedin.com/in/chhea-chhouy" target="_blank" class="w-12 h-12 sm:w-14 sm:h-14 rounded-xl bg-white/10 border border-white/20 flex items-center justify-center hover:bg-indigo-500/20 hover:border-indigo-500 transition group">
+                <a href="https://www.linkedin.com/in/chhea-chhouy-337667374/" target="_blank" class="w-12 h-12 sm:w-14 sm:h-14 rounded-xl bg-white/10 border border-white/20 flex items-center justify-center hover:bg-indigo-500/20 hover:border-indigo-500 transition group">
                   <svg class="w-6 h-6 sm:w-7 sm:h-7 text-indigo-400 group-hover:text-indigo-300" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
                 </a>
 
